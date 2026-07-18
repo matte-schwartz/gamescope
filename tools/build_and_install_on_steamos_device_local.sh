031517d20886d32a6e2b09231ca739d6d841b486
Autumn Ashton <misyl@froggi.es>
2026-07-18 03:59:56 +0100

    tools: Install all runtime contents in our install scripts
    --tags runtime exists and solves what this comment brought up.

--- tools/build_and_install_on_steamos_device_local.sh
+++ tools/build_and_install_on_steamos_device_local.sh
@@ -14,11 +14,6 @@ envsudo steamos-readonly disable
 
 steamvr stop
 
-# One option is to do this, but this also installs a bunch of headers and rubbish
-# that we don't want.
-#envsudo meson install --skip-subprojects -C build.local
-
-# So just install the main gamescope executable for now...
-envsudo cp -ra build.local/src/gamescope /usr/bin/gamescope
+envsudo meson install --skip-subprojects --tags runtime -C build.local
 
 steamvr start
