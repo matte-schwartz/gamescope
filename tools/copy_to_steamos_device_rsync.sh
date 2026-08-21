06adfa8d7e3504dfca65eeb3e223983f8aa5a7ed
Matthew Schwartz <matthew.schwartz@linux.dev>
2026-08-21 11:08:46 -0700

    tools: fix quotes in paths and passwords breaking our deploy tools

--- tools/copy_to_steamos_device_rsync.sh
+++ tools/copy_to_steamos_device_rsync.sh
@@ -6,6 +6,6 @@ source "$(dirname "$0")/steamos_common_remote.sh" "$@"
 
 pushd ..
 echo "Copying $(pwd) to $STEAMOS_DEVICE_IP:$STEAMOS_DEVICE_PATH..."
-envsshpass rsync -rzah --delete --info=progress2 --exclude '/build*' --exclude '.git' \
-    --rsync-path="mkdir -p '$STEAMOS_DEVICE_PATH' && rsync" \
+envsshpass rsync -rzahs --delete --info=progress2 --exclude '/build*' --exclude '.git' \
+    --rsync-path="mkdir -p $(envquote "$STEAMOS_DEVICE_PATH") && rsync" \
     . "steamos@$STEAMOS_DEVICE_IP:$STEAMOS_DEVICE_PATH"
